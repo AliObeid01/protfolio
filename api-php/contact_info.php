74ec7a9f1842fdd0502b5295839cefda0f4d333b
unknown <aliiobeid01@gmail.com>
2022-09-14 04:44:12 +0300

    implement the get api of contact information

---
+++ api-php/contact_info.php
@@ -0,0 +1,23 @@
+<?php
+
+header('Access-Control-Allow-Origin: http://localhost/');
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
+include("connection.php");
+
+$query = $mysqli->prepare("SELECT name, email, phone, message FROM contact_info");
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+
+while($info = $array->fetch_assoc()){
+    $response[] = $info;
+}
+
+$json = json_encode($response);
+echo $json;
+
+?>
