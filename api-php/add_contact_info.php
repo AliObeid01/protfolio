cece28e90c6533ce641fe72891299ad7dd7eeb87
unknown <aliiobeid01@gmail.com>
2022-09-14 05:07:48 +0300

    implement the post api to add the contact infomration to the database

---
+++ api-php/add_contact_info.php
@@ -0,0 +1,24 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
+include("connection.php");
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$message = $_POST["message"];
+
+
+$query = $mysqli->prepare("INSERT INTO contact_info(name, email, phone, message) VALUE (?, ?, ?, ?)");
+$query->bind_param("ssss", $name, $email, $phone, $message);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
